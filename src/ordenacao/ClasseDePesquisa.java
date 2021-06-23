6fc13dc59c04c02026099402c7c5d0c4a6ff4a63
marcel <marcel@MC-5155.furb.br>
2021-06-23 08:48:31 -0300

    Unidade 6 - ClasseDePesquisa

--- src/ordenacao/ClasseDePesquisa.java
+++ src/ordenacao/ClasseDePesquisa.java
@@ -1,14 +1,32 @@
 package ordenacao;
 
 public class ClasseDePesquisa<T extends Comparable<T>> {
-	
+
 	public int pesquisaLinear(T[] vetor, T elem) {
-		// TODO
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i].compareTo(elem) == 0) {
+				return i;
+			}
+		}
 		return -1;
 	}
-	
+
 	public int buscaBinaria(T[] vetor, T elem) {
-		// TODO
+		int inicio = 0;
+		int fim = vetor.length - 1;
+		int metade;
+
+		while (inicio <= fim) {
+			metade = ((inicio + fim) / 2);
+			if (vetor[metade].compareTo(elem) > 0) {
+				fim = metade-1;
+			} else if (vetor[metade].compareTo(elem) < 0) {
+				inicio = metade+1;
+			} else {
+				return metade;
+			}
+		}
 		return -1;
-    }
+	}
+
 }
